dc348d593329e3bfc62fe1b8995cddd6629dd1bc
Humza <humza@utmuser157-135.wireless.utoronto.ca>
2019-10-02 16:23:26 -0400

    Added function that prints board on the screen

--- TicTacToeController.py
+++ TicTacToeController.py
@@ -7,10 +7,27 @@ def insertSymbol(symbol, pos):
      return board[pos] == ' '
 
 def displayBoard(board):
-    pass
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
 
-def isWinner(no, le):
-    pass
+def isWinner(board, symbol):
+    return (board[7] == symbol and board[8] == symbol and board[9] == symbol) or
+    (board[4] == symbol and board[5] == symbol and board[6] == symbol) or
+    (board[1] == symbol and board[2] == symbol and board[3] == symbol) or
+    (board[1] == symbol and board[4] == symbol and board[7] == symbol) or
+    (board[2] == symbol and board[5] == symbol and board[8] == symbol) or
+    (board[3] == symbol and board[6] == symbol and board[9] == symbol) or
+    (board[1] == symbol and board[5] == symbol and board[9] == symbol) or 
+    (board[3] == symbol and board[5] == symbol and board[7] == symbol)
 
 def playerMove():
     pass
