603768f9b65587a1511bc6dde0bf6e51f09091f6
Humza <humza@utmuser157-135.wireless.utoronto.ca>
2019-10-02 13:46:30 -0400

    Defined functions for the game

---
+++ TicTacToeController.py
@@ -0,0 +1,28 @@
+def insertSymbol(symbol, pos):
+  pass
+
+ def isSpaceFree(pos):
+     pass
+
+def displayBoard(board):
+    pass
+
+def isWinner(no, le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove():
+    pass
+
+def selectRandom(board):
+    pass
+
+def isBoardFull(board):
+    pass
+
+def main():
+    pass
+
+main()
